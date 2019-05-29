6c596f27619397ef9dc320197fb9514a906c5f31
Gongo <jvilla1714@gmail.com>
2019-05-29 12:03:36 -0400

    Changed the output pattern

--- Board.X/IOPattern.asm
+++ Board.X/IOPattern.asm
@@ -20,20 +20,57 @@ MainLoop:
     BTG PORTA,RA5 ;Toggle PORTA PIN 7(LED's)
     BTG PORTA,RA4 ;Toggle PORTA PIN 7(LED's)
     BTFSS PORTB, RB4 ; If button is pressed, go to other label
-    GOTO Delay
+    GOTO Delay_one
     GOTO MainLoop
     
-OtherPattern:
+LED1:
     BTG PORTA,RA7 ;Toggle PORTA PIN 7(LED's)
+    BTFSS PORTB, RB4 ; If button is pressed, go to other label
+    GOTO Delay_two
+    GOTO Start
+LED2:
+    BTG PORTA,RA7
     BTG PORTA,RA6 ;Toggle PORTA PIN 7(LED's)
-    GOTO OtherPattern
-    
-Delay:
+    BTFSS PORTB, RB4 ; If button is pressed, go to other label
+    GOTO Delay_three
+    GOTO Start
+LED3:
+    BTG PORTA,RA6
+    BTG PORTA,RA5 ;Toggle PORTA PIN 7(LED's)
+    BTFSS PORTB, RB4 ; If button is pressed, go to other label
+    GOTO Delay_four
+    GOTO Start
+LED4:
+    BTG PORTA,RA5
+    BTG PORTA,RA4 ;Toggle PORTA PIN 7(LED's)
+    BTFSS PORTB, RB4 ; If button is pressed, go to other label
+    GOTO Delay_one
+    GOTO Start
+
+Delay_one:
     DECFSZ Delay1,1 ;Decrement Delay1 by 1, skip next instruction if Delay1 is 0
-    GOTO Delay
+    GOTO Delay_one
     DECFSZ Delay2,1
-    GOTO Delay
-    GOTO MainLoop
+    GOTO Delay_one
+    GOTO LED1
+Delay_two:
+    DECFSZ Delay1,1 ;Decrement Delay1 by 1, skip next instruction if Delay1 is 0
+    GOTO Delay_two
+    DECFSZ Delay2,1
+    GOTO Delay_two
+    GOTO LED2
+Delay_three:
+    DECFSZ Delay1,1 ;Decrement Delay1 by 1, skip next instruction if Delay1 is 0
+    GOTO Delay_three
+    DECFSZ Delay2,1
+    GOTO Delay_three
+    GOTO LED3
+Delay_four:
+    DECFSZ Delay1,1 ;Decrement Delay1 by 1, skip next instruction if Delay1 is 0
+    GOTO Delay_four
+    DECFSZ Delay2,1
+    GOTO Delay_four
+    GOTO LED4
 end
 
 
